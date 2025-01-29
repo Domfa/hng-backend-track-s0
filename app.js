67255e74421e00f561c63f7989556dbccf1da9ac
Domfa <jdomfajohnson@gmail.com>
2025-01-29 13:16:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/api/v1', (req, res) => {
+  const response = {
+    email: 'jjoshuadomfa@gmail.com',
+    current_datetime: new Date().toISOString(),
+    github_url: 'https://github.com/Domfa/hng-backend-track-s0.git',
+  };
+  res.status(200).json(response);
+});
+
+app.listen(port, () => {
+  console.log(`app is listening on port: ${port}`);
+});
