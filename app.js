fe7b5e648f22e942cc29c8b94274c150375093db
Joshua Domfa Johnson <85454314+Domfa@users.noreply.github.com>
2025-01-29 14:14:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
   const response = {
     email: 'jjoshuadomfa@gmail.com',
     current_datetime: new Date().toISOString(),
